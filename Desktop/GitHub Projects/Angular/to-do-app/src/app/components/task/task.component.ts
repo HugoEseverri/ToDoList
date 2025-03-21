f51987376c76d699e4a31a07f8d9bb4be48dabe5
Hugo Eseverri <hugooeseverri@gmail.com>
2025-03-21 15:32:37 -0300

    task ts listo

--- Desktop/GitHub Projects/Angular/to-do-app/src/app/components/task/task.component.ts
+++ Desktop/GitHub Projects/Angular/to-do-app/src/app/components/task/task.component.ts
@@ -1,26 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.scss']
 })
-export class TaskComponent {
-  tasks: { text: string, completed: boolean }[] = [];
-  newTask: string = '';
+export class TaskComponent{
+  @Input() task!: {text:string; completed: boolean; createdAt: Date}
+  @Input() index!: number;
 
-  addTask() {
-    if (this.newTask.trim()) {
-      this.tasks.push({ text: this.newTask, completed: false });
-      this.newTask = '';
-    }
+  constructor(private taskService: TaskService) {}
+
+  toggleTask() {
+    this.taskService.toggleTask(this.index);
   }
 
-  toggleTask(index: number) {
-    this.tasks[index].completed = !this.tasks[index].completed;
-  }
-
-  deleteTask(index: number) {
-    this.tasks.splice(index, 1);
+  deleteTask() {
+    this.taskService.deleteTask(this.index);
   }
 }
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-task',
+//   templateUrl: './task.component.html',
+//   styleUrls: ['./task.component.css']
+// })
+// export class TaskComponent {
+//   tasks: { text: string, completed: boolean }[] = [];
+//   newTask: string = '';
+
+//   addTask() {
+//     if (this.newTask.trim()) {
+//       this.tasks.push({ text: this.newTask, completed: false });
+//       this.newTask = '';
+//     }
+//   }
+
+//   toggleTask(index: number) {
+//     this.tasks[index].completed = !this.tasks[index].completed;
+//   }
+
+//   deleteTask(index: number) {
+//     this.tasks.splice(index, 1);
+//   }
+// }
