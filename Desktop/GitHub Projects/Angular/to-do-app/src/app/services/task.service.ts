a8d14d02d82403273041bc38aeccad8304846b1f
Hugo Eseverri <hugooeseverri@gmail.com>
2025-03-21 15:20:16 -0300

    Task Service completo

---
+++ Desktop/GitHub Projects/Angular/to-do-app/src/app/services/task.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TaskService {
+    private tasks: { text: string; completed: boolean; createdAt: Date }[]=[]
+
+    constructor(){
+        this.loadTasks();
+    }
+
+    addTask(text: string) {
+        this.tasks.push({ text, completed: false, createdAt: new Date() });
+        this.saveTasks();
+    }
+
+    toggleTask(index: number) {
+        this.tasks[index].completed = !this.tasks[index].completed;
+        this.saveTasks()
+    }
+    
+    deleteTask(index: number) {
+        this.tasks.splice(index, 1);
+        this.saveTasks();
+    }
+
+    getTasks(){
+        return this.tasks;
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
+    private loadTasks(){
+        const storedTasks = localStorage.getItem('tasks');
+        if (storedTasks) {
+            this.tasks = JSON.parse(storedTasks);
+        }
+    }
+}
